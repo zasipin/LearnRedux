6b013a447e0f895a868ab20ede1c0fd8e7dbb452
zasipin <zasipin.it@gmail.com>
2017-03-10 20:15:14 +0300

    pure function example

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,24 @@
 import redux from 'redux';
 
 console.log('running redux app');
+
+// // pure function
+// function add(a, b){
+//   return a + b;
+// }
+//
+// function changeProp(obj){
+//   return {
+//     ...obj,
+//     name: 'Jen'
+//   }
+// }
+//
+// var startingValue = {
+//   name: 'Andrew',
+//   age: 27
+// }
+//
+// var res = changeProp(startingValue);
+// console.log(startingValue);
+// console.log(res);
