6afe56d7f6b140b142d3444030dfa9d389ec3132
zasipin <zasipin.it@gmail.com>
2017-03-11 15:48:06 +0300

    Add actions for adding/removing hobbies, movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,35 +1,115 @@
-import * as redux from 'redux';
-// var redux = require('redux');
-console.log('running redux app');
+var redux = require('redux');
 
-// reducer - pure function
-var reducer = (state = {name: 'Anonymous'}, action) => {
-  // state = state || {name: 'Anonymous'};
-  return state;
+console.log('Starting redux example');
+
+var hobbyId = 1,
+    movieId = 1;
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
 };
 
-var store = redux.createStore(reducer);
+var reducer = (state = stateDefault, action) => {
+  // state = state || {name: 'Anonymous'};
+
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    case 'ADD_HOBBY':
+        return {
+          ...state,
+          hobbies: [
+            ...state.hobbies,
+            {
+              id: hobbyId++,
+              hobby: action.hobby
+            }
+          ]
+        }
+    case 'REMOVE_HOBBY':
+          return {
+            ...state,
+            hobbies: state.hobbies.filter((hobby)=> hobby.id !== action.id)
+          }
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: movieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      }
+    case 'REMOVE_MOVIE':
+            return {
+              ...state,
+              movies: state.hobbies.filter((item)=> item.id !== action.id)
+            }
+    default:
+      return state;
+  }
+};
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  console.log('state', state);
+  document.getElementById('app').innerHTML = state.name;
+});
+// unsubscribe();
 
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-// // pure function
-// function add(a, b){
-//   return a + b;
-// }
-//
-// function changeProp(obj){
-//   return {
-//     ...obj,
-//     name: 'Jen'
-//   }
-// }
-//
-// var startingValue = {
-//   name: 'Andrew',
-//   age: 27
-// }
-//
-// var res = changeProp(startingValue);
-// console.log(startingValue);
-// console.log(res);
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Andrew'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'soome',
+  genre: 'genre1'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'so12ome',
+  genre: 'genre3'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+})
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Emily'
+});
